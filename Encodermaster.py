03ea939127683de873d12879daf26850c56457ad
Zeynalovilkin <ilkin.zeynalovich@gmail.com>
2017-02-02 13:42:27 +0400

    Create Encodermaster.py

---
+++ Encodermaster.py
@@ -0,0 +1,24 @@
+import hashlib
+
+
+
+def Credit():
+    print('#############################')
+    print('##                         ##')
+    print('#  ***Magic Encoder v1***   #')
+    print('# Script by Pentester Rekax #')
+    print('##                         ##')
+    print('#############################')
+Credit()
+
+
+parol = input("Set Password: ")
+
+m=hashlib.md5(('test').encode('utf-8')).hexdigest()
+
+a = (parol)
+hashing = m
+hash256=hashlib.sha256(hashing.encode('utf-8')).hexdigest()
+#bash = hash.hexdigest()
+has = hashlib.md5(hash256.encode('utf-8')).hexdigest()
+print (has)
